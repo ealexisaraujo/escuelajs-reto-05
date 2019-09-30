b368794482774097391f68b940ba3c47c4a6a477
Alexis Araujo <ala1289@gmail.com>
2019-09-30 09:11:19 -0500

    Reto solucionados

--- src/index.js
+++ src/index.js
@@ -1,38 +1,54 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://rickandmortyapi.com/api/character/';
+const storage = window.localStorage;
+storage.setItem('next_fetch', API);
 
 const getData = api => {
   fetch(api)
     .then(response => response.json())
     .then(response => {
+      storage.setItem('next_fetch', response.info.next);
       const characters = response.results;
-      let output = characters.map(character => {
-        return `
+      let output = characters
+        .map(character => {
+          return `
       <article class="Card">
         <img src="${character.image}" />
         <h2>${character.name}<span>${character.species}</span></h2>
       </article>
-    `
-      }).join('');
+    `;
+        })
+        .join('');
       let newItem = document.createElement('section');
       newItem.classList.add('Items');
       newItem.innerHTML = output;
       $app.appendChild(newItem);
     })
     .catch(error => console.log(error));
-}
+};
 
-const loadData = () => {
-  getData(API);
-}
+const loadData = async () => {
+  const nextAPI = storage.getItem('next_fetch');
+  const api = nextAPI !== null ? nextAPI : API;
 
-const intersectionObserver = new IntersectionObserver(entries => {
-  if (entries[0].isIntersecting) {
-    loadData();
+  if (api === '') {
+    $observe.innerHTML = '<h2>Ya no hay personajes...</h2>';
+    intersectionObserver.unobserve($observe);
+  } else {
+    await getData(api);
   }
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+};
+
+const intersectionObserver = new IntersectionObserver(
+  entries => {
+    if (entries[0].isIntersecting) {
+      loadData();
+    }
+  },
+  {
+    rootMargin: '0px 0px 100% 0px'
+  }
+);
 
 intersectionObserver.observe($observe);
